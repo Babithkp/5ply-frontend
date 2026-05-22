e5f9569d5c3f3fe73215f311d3c7ed5040677c74
Babithkp <babithkullachetti44@gmail.com>
2026-05-22 19:43:04 +0530

    new updated for 2026

--- src/Home.tsx
+++ src/Home.tsx
@@ -93,7 +93,7 @@ function Home() {
       title: "Andal Packaging Industries",
       description:
         "5-step, 2.5ft x 2.5ft x 2.5ft, 0.5ft per step. Heavy-duty, DIY, foldable. Supports up to 20 kg per step.",
-      price: parseInt(quantity) * 1,
+      price: parseInt(quantity) * 1300,
     };
 
     const orderCreation = await initialOrderCreate(item);
